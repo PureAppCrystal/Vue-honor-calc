aedfa6e859d8043863e4a279c42b73a3af9a931a
배상훈 <bsh0814@bridgetec.co.kr>
2019-12-27 17:31:03 +0900

    1차 완성

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,20 @@
       
         <v-row>
           <v-col cols="12" md="12"  id='content-top'>
-            <img alt="Vue logo" src="./assets/logo.png">
+            <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+            <div class="main-title">WOW Classic Honor RP Calculator</div>
           </v-col>
-          <v-col cols="12" md="5"  id='content-left'>
+          <v-col cols="12" md="6"  id='content-left'>
             <v-row style="display: flex; justify-content: center;">
               <v-col cols="12" sm="7" md="12" class="content-box">
-                <div class="box-title"> aa</div>
+                <div class="box-title"> 기본정보</div>
                 <v-row>
-                  <v-col cols="2">label</v-col>
-                  <v-col cols="4">
+                  <v-col cols="4" class="label">서버인구</v-col>
+                  <v-col cols="8">
                     <input type="number" class="value-input" v-model="totalPeople"  @keyup="checkNumber($event)"/>
                   </v-col>
-                  <v-col cols="1">label</v-col>
-                  <v-col cols="5">
+                  <v-col cols="4" class="label">진영</v-col>
+                  <v-col cols="8">
                     <v-select
                         v-model="selectCamp" 
                         :items="campList" 
@@ -30,62 +31,67 @@
                 </v-row>
               </v-col>
               <v-col cols="12" sm="7" md="12" class="content-box">
-                <div class="box-title"> aa</div>
+                <div class="box-title">예상결과</div>
                 <v-row>
-                  <v-col cols="4">label</v-col>
-                  <v-col cols="8"><input type="number" class="value-input" v-model="expectRank" @keyup="checkNumber($event)"/></v-col>
+                  <v-col cols="4" class="label">순위</v-col>
+                  <v-col cols="4"><input type="number" class="value-input" v-model="expectRank" @keyup="checkNumber($event)"/></v-col>
+                  <v-col cols="4">
+                    <v-switch v-model="nextGetAuto" :label="'AutoSet'" class="value-switch tooltip">
+                    </v-switch>
+                  </v-col>
                 </v-row>
                 <v-row>
-                  <v-col cols="4">label</v-col>
-                  <v-col cols="4">{{expectResult.exp}}</v-col>
-                  <v-col cols="4">{{expectResult.braket}}</v-col>
-                </v-row>
-              </v-col>
-              <v-col cols="12" sm="7" md="12" class="content-box">
-                <div class="box-title"> aa</div>
-                <v-row>
-                  <v-col cols="4">label</v-col>
-                  <v-col cols="4"><input type="number" class="value-input" /></v-col>
-                  <v-col cols="4">value</v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="4">label</v-col>
-                  <v-col cols="4"><input type="number" class="value-input"/></v-col>
-                  <v-col cols="4">value</v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="4">label</v-col>
-                  <v-col cols="8">value</v-col>
+                  <v-col cols="4" class="label">점수</v-col>
+                  <v-col cols="4" class="result">{{expectResult.exp}}</v-col>
+                  <v-col cols="4" class="braket">{{expectResult.braket}}</v-col>
                 </v-row>
               </v-col>
               <v-col cols="12" sm="7" md="12" class="content-box">
-                <div class="box-title"> aa</div>
+                <div class="box-title">결과 확인</div>
                 <v-row>
-                  <v-col cols="4">label</v-col>
-                  <v-col cols="4"><input type="number" class="value-input"/></v-col>
-                  <v-col cols="4">value</v-col>
+                  <v-col cols="4" class="label">현재 경험치</v-col>
+                  <v-col cols="4"><input type="number" class="value-input" v-model="nextNow" @keyup="checkNumber($event)"/></v-col>
+                  <v-col cols="4" class="grade">{{nextNowGrade}}</v-col>
                 </v-row>
                 <v-row>
-                  <v-col cols="4">label</v-col>
-                  <v-col cols="8"><input type="number" class="value-input"/></v-col>
+                  <v-col cols="4" class="label">획득 경험치</v-col>
+                  <v-col cols="8"><input type="number" class="value-input" v-model="nextGet" @keyup="checkNumber($event)"/></v-col>
                 </v-row>
                 <v-row>
-                  <v-col cols="4">label</v-col>
-                  <v-col cols="4">value</v-col>
-                  <v-col cols="4">value</v-col>
+                  <v-col cols="4" class="label">결과 경험치</v-col>
+                  <v-col cols="3" class="result">{{nextExp}}</v-col>
+                  <v-col cols="4" class="grade">{{nextExpGrade}}</v-col>
                 </v-row>
               </v-col>
+              <v-col cols="12" sm="7" md="12" class="content-box">
+                <div class="box-title"> 필요 경험치 계산</div>
+                <v-row>
+                  <v-col cols="4" class="label">현재 경험치</v-col>
+                  <v-col cols="4"><input type="number" class="value-input" v-model="needNow" @keyup="checkNumber($event)"/></v-col>
+                  <v-col cols="4" class="grade">{{needNowGrade}}</v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="4" class="label">목표 경험치</v-col>
+                  <v-col cols="4"><input type="number" class="value-input" v-model="needTarget" @keyup="checkNumber($event)"/></v-col>
+                  <v-col cols="4" class="grade">{{needTargetGrade}}</v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="4" class="label">필요 경험치</v-col>
+                  <v-col cols="8" class="result">{{needExp}}</v-col>
+                </v-row>
+              </v-col>
+              
             </v-row>
           </v-col>
-          <v-col cols="12" md="7"  id='content-right'>
+          <v-col cols="12" md="6"  id='content-right'>
             <v-row>
               <v-col cols="12"  class="content-box">
-                <div class="box-title"> aa</div>
-                <img alt="Vue logo" src="./assets/logo.png">
+                <div class="box-title"> 계급도</div>
+                <img alt="Vue logo" src="./assets/img/grade.png" class="value-img">
               </v-col>
               <v-col cols="12"  class="content-box">
-                <div class="box-title"> aa</div>
-                <img alt="Vue logo" src="./assets/logo.png">
+                <div class="box-title"> Braket</div>
+                <img alt="Vue logo" src="./assets/img/braket.png" class="value-img">
               </v-col>
             </v-row>
           </v-col>  
@@ -105,9 +111,9 @@ export default {
 
   data() {
     return {
-      totalPeople: 6000,
-      selectCamp: {CODE_CD:0, CODE_NAME:"data1"},
-      campList:[{CODE_CD:0, CODE_NAME:"data1"},{CODE_CD:1, CODE_NAME:"data2"}],
+      totalPeople: 4000,
+      selectCamp: {CODE_CD:2, CODE_NAME:"호드"},
+      campList:[{CODE_CD:1, CODE_NAME:"얼라이언스"},{CODE_CD:2, CODE_NAME:"호드"}],
 
       expectRank: 1,
       //expectExp: 0,
@@ -119,10 +125,12 @@ export default {
       // needExp: 0,
       nextNow: 0,
       nextGet: 0,
+      nextGetAuto: true,
       // netxep: 0,
     }
   },
   computed: {
+    //braket
     expectResult: function() {
       const {totalPeople, expectRank } = this;
       // data check
@@ -132,66 +140,34 @@ export default {
 
       let percent = expectRank / totalPeople * 100;
       percent = (Math.ceil(percent * 100 ))/100; // 3자리에서 올림
-      console.log("percent : ", percent);
-      let result = {exp: 0, braket: "-" };
+      return this.getBraket(percent);
+    },
 
-      if( percent<=0.3) {
-        result.braket = "braket14"
-        result.exp = 12000
-      } else if ( percent <= 0.8 ) {
-        result.braket = "braket13"
-        result.exp = 11000
-      } else if ( percent <= 2.0 ) {
-        result.braket = "braket12"
-        result.exp = 10000
-      } else if ( percent <= 3.5 ) {
-        result.braket = "braket11"
-        result.exp = 9000
-      } else if ( percent <= 6.0 ) {
-        result.braket = "braket10"
-        result.exp = 8000
-      } else if ( percent <= 10.0 ) {
-        result.braket = "braket9"
-        result.exp = 7000
-      } else if ( percent <= 15.9 ) {
-        result.braket = "braket8"
-        result.exp = 6000
-      } else if ( percent <= 22.8 ) {
-        result.braket = "braket7"
-        result.exp = 5000
-      } else if ( percent <= 32.7 ) {
-        result.braket = "braket6"
-        result.exp = 4000
-      } else if ( percent <= 43.6 ) {
-        result.braket = "braket5"
-        result.exp = 3000
-      } else if ( percent <= 56.6 ) {
-        result.braket = "braket4"
-        result.exp = 2000
-      } else if ( percent <= 69.7 ) {
-        result.braket = "braket3"
-        result.exp = 1000
-      } else if ( percent <= 84.5 ) {
-        result.braket = "braket2"
-        result.exp = 400
-      } else {
-        result.braket = "braket1"
-        result.exp = 0
-      }
-
-      if ( expectRank == 1 ) {
-        result.exp = 13000
-      }
-
-      return result
+    // need
+    needNowGrade: function() {
+      return this.getGrade(this.needNow);
+    },
+    needTargetGrade: function() {
+      return this.getGrade(this.needTarget);
     },
     needExp: function(){
       const { needNow, needTarget } = this;
-      return  parseInt(needTarget) - (needNow * 0.8);
+      const result = parseInt(needTarget) - (needNow * 0.8);
+      return isNaN(result) ? 0 : Math.ceil(result);
+    },
+
+
+    // next
+    nextNowGrade: function() {
+      return this.getGrade(this.nextNow);
     },
     nextExp: function() {
       const { nextNow, nextGet } = this;
-      return  (nextNow * 0.8 ) + parseInt(nextGet);
+      const result =  (nextNow * 0.8 ) + parseInt(nextGet);
+      return isNaN(result) ? 0 : Math.ceil(result);
+    },
+    nextExpGrade: function() {
+      return this.getGrade(this.nextExp);
     } 
   },
   methods: {
@@ -201,6 +177,112 @@ export default {
       const value = parseInt(event.target.value)
       event.target.value = isNaN(value) ? 0 : value;
     },
+    getGrade(exp) {
+      // const {selectCamp} = this;
+      const selectCamp = this.selectCamp.CODE_CD;
+      let grade 
+      if ( exp >= 60000) {
+        grade = selectCamp === 1 ? "최고사령관" : "대장군"
+      } else if ( exp >= 55000 ) {
+        grade = selectCamp === 1 ? "야전사령관" : "장군"
+      } else if ( exp >= 50000 ) {
+        grade = selectCamp === 1 ? "작전사령관" : "전투사령관"
+      } else if ( exp >= 45000 ) {
+        grade = selectCamp === 1 ? "사령관" : "사령관"
+      } else if ( exp >= 40000 ) {
+        grade = selectCamp === 1 ? "부사령관" : "부사령관"
+      } else if ( exp >= 35000 ) {
+        grade = selectCamp === 1 ? "기사단장" : "백인대장"
+      } else if ( exp >= 30000 ) {
+        grade = selectCamp === 1 ? "기사대장" : "용사"
+      } else if ( exp >= 25000 ) {
+        grade = selectCamp === 1 ? "상급기사" : "혈투사"
+      } else if ( exp >= 20000 ) {
+        grade = selectCamp === 1 ? "기사" : "투사"
+      } else if ( exp >= 15000 ) {
+        grade = selectCamp === 1 ? "하급기사" : "하급투사"
+      } else if ( exp >= 10000 ) {
+        grade = selectCamp === 1 ? "정예근위병" : "정예수호병"
+      } else if ( exp >= 5000 ) {
+        grade = selectCamp === 1 ? "근위병" : "수호병"
+      } else if ( exp >= 2000 ) {
+        grade = selectCamp === 1 ? "파수병" : "그런트"
+      } else {
+        grade = selectCamp === 1 ? "정찰병" : "척후병"
+      }
+
+      return grade;
+    },
+    getBraket: function(percent) {
+      const {totalPeople, expectRank } = this;
+      let result = {exp: 0, braket: "-" };
+
+      if( percent<=0.3) {
+        result.braket = "braket14"
+        result.exp = 13000 - Math.floor(1000/(totalPeople * 0.003)) * expectRank
+      } else if ( percent <= 0.8 ) {
+        result.braket = "braket13"
+        const beforePeople = totalPeople * 0.003;
+        result.exp = 12000 - Math.floor(1000/(totalPeople * 0.008 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 2.0 ) {
+        result.braket = "braket12"
+        const beforePeople = totalPeople * 0.008;
+        result.exp = 11000 - Math.floor(1000/(totalPeople * 0.02 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 3.5 ) {
+        result.braket = "braket11"
+        const beforePeople = totalPeople * 0.02;
+        result.exp = 10000 - Math.floor(1000/(totalPeople * 0.035 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 6.0 ) {
+        result.braket = "braket10"
+        const beforePeople = totalPeople * 0.035;
+        result.exp = 9000 - Math.floor(1000/(totalPeople * 0.06 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 10.0 ) {
+        result.braket = "braket9"
+        const beforePeople = totalPeople * 0.06;
+        result.exp = 8000 - Math.floor(1000/(totalPeople * 0.10 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 15.9 ) {
+        result.braket = "braket8"
+        const beforePeople = totalPeople * 0.10;
+        result.exp = 7000 - Math.floor(1000/(totalPeople * 0.159 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 22.8 ) {
+        result.braket = "braket7"
+        const beforePeople = totalPeople * 0.159;
+        result.exp = 6000 - Math.floor(1000/(totalPeople * 0.228 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 32.7 ) {
+        result.braket = "braket6"
+        const beforePeople = totalPeople * 0.228;
+        result.exp = 5000 - Math.floor(1000/(totalPeople * 0.327 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 43.6 ) {
+        result.braket = "braket5"
+        const beforePeople = totalPeople * 0.327;
+        result.exp = 4000 - Math.floor(1000/(totalPeople * 0.436 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 56.6 ) {
+        result.braket = "braket4"
+        const beforePeople = totalPeople * 0.436;
+        result.exp = 3000 - Math.floor(1000/(totalPeople * 0.566 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 69.7 ) {
+        result.braket = "braket3"
+        const beforePeople = totalPeople * 0.566;
+        result.exp = 2000 - Math.floor(1000/(totalPeople * 0.697 - beforePeople)) * (expectRank - beforePeople)
+      } else if ( percent <= 84.5 ) {
+        result.braket = "braket2"
+        const beforePeople = totalPeople * 0.697;
+        result.exp = 1000 - Math.floor(600/(totalPeople * 0.845 - beforePeople)) * (expectRank - beforePeople)
+      } else {
+        result.braket = "braket1"
+        result.exp = 0
+      }
+
+      if ( expectRank == 1 ) {
+        result.exp = 13000
+      }
+      result.exp = Math.floor(result.exp);
+      if(this.nextGetAuto) {
+        this.nextGet = result.exp
+      }
+
+      return result
+    }
 
   }
 };
